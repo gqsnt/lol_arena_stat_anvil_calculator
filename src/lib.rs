ec30c29b92131dd76c5fb8ce804d5c7b93bb5278
gquesnot <gaqu1994@gmail.com>
2024-05-26 12:22:46 +0200

    fix float format

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,8 @@ pub mod stat_shard;
 pub mod player_stats;
 
 pub type StatShardPercent = (StatShard, f64);
+
+
+pub fn trim_float(f: f64) -> String {
+    format!("{:.2}", f).trim_end_matches('0').trim_end_matches('.').to_string()
+}
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,18 @@ use dialoguer::Select;
 use dialoguer::theme::ColorfulTheme;
 
 use lol_stat_shard::player_stats::PlayerStats;
+use lol_stat_shard::trim_float;
 
 fn main() {
     let mut default_stats = PlayerStats::new();
     loop {
-        println!("Stat roll count: {:.2}", default_stats.total_shard);
+        println!("Stat roll count: {}", default_stats.total_shard);
         if default_stats.used_pristine {
             println!("Pristine used");
         }
-        println!("Current stats: {}", default_stats.calculate_total_gold_value());
+        println!("Current stats: {} gold", trim_float(default_stats.calculate_total_gold_value()));
         for (stat, value) in &default_stats.hash_map {
-            println!("\t{:?}: {:.2}", stat, value);
+            println!("\t{:?}: {}", stat, trim_float(*value));
         }
         let menu_items = vec!["Roll", "Reset", "Stop"];
         let selection = Select::with_theme(&ColorfulTheme::default())
@@ -25,7 +26,7 @@ fn main() {
         match menu_items[selection] {
             "Roll" => {
                 let shards = default_stats.get_3_choices();
-                let shard_descriptions: Vec<String> = shards.iter().map(|(shard, percent)| format!("{:?} ({}%)", shard, percent * 100.0)).collect();
+                let shard_descriptions: Vec<String> = shards.iter().map(|(shard, percent)| format!("{:?} ({}%)", shard, (percent * 100.0) as i32)).collect();
                 let shard_selection = Select::with_theme(&ColorfulTheme::default())
                     .with_prompt("Choose a StatShard")
                     .default(0)
